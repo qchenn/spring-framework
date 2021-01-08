e202e464cd03138d40ea09d97d943b2a26c63494
陈前 <1141306007@qq.com>
2021-01-08 20:48:53 +0800

    获取Vm字符编码方式

--- spring-mytest/src/main/java/com/qc/Test.java
+++ spring-mytest/src/main/java/com/qc/Test.java
@@ -2,11 +2,14 @@ package com.qc;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.nio.charset.Charset;
+
 public class Test {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(UserConfig.class);
 		User user = (User) context.getBean("user");
+		System.err.println(Charset.defaultCharset().name());
 		System.err.println(System.getProperty("file.encoding"));
 		System.err.println(user.toString());
 	}
